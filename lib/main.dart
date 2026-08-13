c16b7c87c660d7ec301c2eb9c194e6bb6e295b66
AmroKhaled20 <kamr82460@gmail.com>
2026-08-13 21:17:20 +0300

    feat: setup Hive storage and register adapters

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:scanify_pdf/core/utils/app_router.dart';
+import 'package:scanify_pdf/core/utils/constants.dart';
 import 'package:scanify_pdf/core/utils/service_locator.dart';
 import 'package:scanify_pdf/core/utils/simple_bloc_observer.dart';
+import 'package:scanify_pdf/features/home/domain/entities/pdf_file_entity.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(PdfFileEntityAdapter());
+  await Hive.openBox<PdfFileEntity>(kPdfFilesBox);
   setupServiceLocator();
-
   Bloc.observer = SimpleBlocObserver();
   runApp(const ScanifyPDF());
 }
